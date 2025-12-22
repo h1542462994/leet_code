889705486be4ec51e3f0e0c3f0e002d5a439239c
h1542462994 <t1542462994@outlook.com>
2025-12-22 19:42:47 +0800

    Add: BigInt class

--- src/main/kotlin/org/tty/leet_code/Solution221.kt
+++ src/main/kotlin/org/tty/leet_code/Solution221.kt
@@ -35,28 +35,10 @@ class Solution221 {
             for (i in 0 until rowCount - N + 1) {
                 for (j in 0 until columnCount - N + 1) {
                     // 判断以i,j为左上角的点是否构成正方形
-                    if (matrix[i][j] == '1' && table[i + 1][j + 1] == 1) {
-                        // 判断中间的长方形是否均为1
-                        var flag = true
-                        for (j2 in j + 1 until j + N) {
-                            if (matrix[i][j2] == '0') {
-                                flag = false
-                                break
-                            }
-                        }
-                        if (flag) {
-                            for (i2 in i + 1 until i + N) {
-                                if (matrix[i2][j] == '0') {
-                                    flag = false
-                                    break
-                                }
-                            }
-                        }
-                        if (flag) {
-                            table[i][j] = 1
-                            find = true
-                            continue
-                        }
+                    if (matrix[i][j] == '1' && table[i + 1][j + 1] == 1 && table[i][j + 1] == 1 && table[i + 1][j] == 1) {
+                        find = true
+                        table[i][j] = 1
+                        continue
                     }
                     table[i][j] = 0
                 }
